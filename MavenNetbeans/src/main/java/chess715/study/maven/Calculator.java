07f3c2f4a1b81df59d999e26669f5fbe75943ac7
ayrat <ayrat@nok2-test1.npopm.ru>
2016-04-06 18:51:45 +0700

    some changes. (javafx + maven)

---
+++ MavenNetbeans/src/main/java/chess715/study/maven/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess715.study.maven;
+
+/**
+ *
+ * @author ayrat
+ */
+public class Calculator {
+//    public static void main(String[] args) {
+//        System.out.println("2+3="+ (2+3));
+//    }
+}
