5ee0ab8bf67792a48efe4c995ad6091c628debd0
ayrat <ayrat@nok2-test1>
2018-10-17 10:24:50 +0700

    try gradle for javafx(via fxlauncher)

--- AOP/aop-step-2/src/main/java/topor/ServiceAspect.java
+++ AOP/aop-step-2/src/main/java/topor/ServiceAspect.java
@@ -1,6 +1,7 @@
 package topor;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -8,9 +9,14 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class ServiceAspect {
 
-    @Before("execution(* Service.*(..))")
-    public void checkMethod(JoinPoint joinPoint){
-        System.out.println("it is working!!!");
+    @Before("execution(* topor.Service.showMessage(String)) && args(msg)")
+    public void checkMethod(String msg){
+        System.out.println("it is working!!!"+msg);
+    }
+    
+    @After("execution(* topor.Service.*(String[],..))")
+    public void checkMainMethod(JoinPoint joinPoint){
+        System.out.println("it is second method!!!");
     }
    
     
