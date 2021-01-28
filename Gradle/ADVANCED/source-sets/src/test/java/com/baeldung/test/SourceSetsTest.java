555bd17f6cab754682b93865d1d14992cbfa0d70
Ayrat Bik <ajratbik@gmail.com>
2021-01-28 13:09:16 +0700

    update gradle start and advanced projects

---
+++ Gradle/ADVANCED/source-sets/src/test/java/com/baeldung/test/SourceSetsTest.java
@@ -0,0 +1,21 @@
+package com.baeldung.test;
+
+import static org.hamcrest.CoreMatchers.is;//-DEPRECATED
+//import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.baeldung.main.SourceSetsObject;
+
+public class SourceSetsTest {
+
+    @Test
+    public void whenRun_ThenSuccess() {
+
+        SourceSetsObject underTest = new SourceSetsObject("lorem", "ipsum");
+
+        assertThat(underTest.getUser(), is("lorem"));
+        assertThat(underTest.getPassword(), is("ipsum"));
+    }
+}
